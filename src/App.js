34785a0f73ecfa0a95815ae030710a3c3e97f517
dhrubaghosh <dhrubajyoti.ghosh2015@gmail.com>
2022-03-10 13:41:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mt-48 text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
